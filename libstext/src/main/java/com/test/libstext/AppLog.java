aca03ca86694bf79c211a0a141e142256311d4b0
NguyenBNT <wonbonmoto@gmail.com>
2020-12-13 22:05:31 +0700

    update lib

--- libstext/src/main/java/com/test/libstext/AppLog.java
+++ libstext/src/main/java/com/test/libstext/AppLog.java
@@ -1,10 +1,16 @@
 package com.test.libstext;
 
+import android.content.Context;
 import android.util.Log;
+import android.widget.Toast;
 
-class AppLog {
+public class AppLog {
 
     public static void error(String tag, String msg) {
         Log.e(tag, msg);
+    }
+
+    public static void showToast(Context context, String text, int duration) {
+        Toast.makeText(context, text, duration).show();
     }
 }
